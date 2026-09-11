f8e12bd3da4c194f4ec8adbb175464cfbebf3791
Ramy Fakhoury <rfakhoury@nvidia.com>
2026-09-11 18:04:24 +0300

    fix Kingston SSD filesystem type

--- hosts/pc/hardware-configuration.nix
+++ hosts/pc/hardware-configuration.nix
@@ -24,9 +24,10 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
- fileSystems."/SSD_kingston" =
-   { device = "/dev/disk/by-uuid/06E0D489E0D47FFB";
-   };
+  fileSystems."/SSD_kingston" =
+    { device = "/dev/disk/by-uuid/06E0D489E0D47FFB";
+      fsType = "ntfs";
+    };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/629a7c53-6268-4504-92dc-d0ed9255bdae"; }
